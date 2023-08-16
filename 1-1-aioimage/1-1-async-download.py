186f3fe56465eda42abdefe0691f009abc6362b0
sahasawatizz <78100677+sahasawatizz@users.noreply.github.com>
2023-08-16 14:47:54 +0700

    1-1-async-download.py

--- 1-1-aioimage/1-1-async-download.py
+++ 1-1-aioimage/1-1-async-download.py
@@ -1,21 +1,31 @@
+import io
 import time
-from random import randint
-import requests as requests
-from flask import Flask, render_template
+import asyncio
+import aiohttp, aiofiles
+from PIL import Image
 
-app = Flask(__name__)
+unsplash_search_url="https://source.unsplash.com/random/300x300"
 
-def get_xkcd_image():
-    comicid = randint(0,1000)
-    response = requests.get(f'https://xkcd.com/{comicid}/info.0.json')
-    return response.json()['img']
+async def download_image_async(session:aiohttp.ClientSession, num :int =1):
+    print(f"{time.ctime()} - Start download image{num}")
+    async with session.get(unsplash_search_url) as response:
+        if response.status == 200:
+            image_buffer = await response.read()
+            img = Image.open(io.BytesIO(image_buffer))
+            print(f"{time.ctime()} - Image{num} shape:{img.size}")
+    
+    async with aiofiles.open(f'.filename{num}.jpg','wb') as new_file:
+        print(f"{time.ctime()} - Writing to 'filename{num}.jpg' ...")
+        await new_file.write(image_buffer)
+    
+async def download_images_async(n:int=10):
+    async with aiohttp.ClientSession() as session:
+        tasks = [download_image_async(session, i + 1 ) for i in range(n)]
+        _=await asyncio.gather(*tasks)
+    return
 
-@app.get('/comic')
-def hello():
-    start = time.perf_counter()
-    url=get_xkcd_image()
-    end=time.perf_counter()
-    return render_template('index.html',end=end,start=start,urls=[url])
-
-if __name__ == '__main__':
-    app.run(debug=True,port=5555)
+if __name__ == "__main__":
+    tick = time.perf_counter()
+    asyncio.run(download_images_async())
+    tock=time.perf_counter()
+    print(f"{time.ctime()}-elapse:{tock-tick:.2f} seconds")
